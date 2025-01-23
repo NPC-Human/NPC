796b8eaafbdf8605c4397a55f2e73dd18ef8963e
NPC-Human <npcstudio8@outlook.com>
2025-01-23 19:56:10 +0300

    Update NPC V2.lua

--- NPC V2.lua
+++ NPC V2.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NPC-Human/NPC/main/Gamelist"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NPC-Human/NPC/main/Gamelist.lua"))()
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
         loadstring(game:HttpGet(Execute))()
